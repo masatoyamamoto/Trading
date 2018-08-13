497d95e7caf8cb7e883dd1da6a2f3f32203f714b
Masato Max Yamamoto <masato.yamamoto10@gmail.com>
2018-08-12 23:12:54 -0500

    changed main.py

--- main.py
+++ main.py
@@ -1,10 +1,8 @@
 import numpy as np
-import pandas_datareader.data as web
+import pandas as pd
 import datetime
 
 start = datetime.datetime(2018, 2, 1)
 end = datetime.datetime(2018, 7, 31)
 
-f = web.DataReader("AAPL", "google", start, end)
 
-print(f.head())
