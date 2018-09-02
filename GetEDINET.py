4c442bc18fcf48568de4227a65eec49e4ac257f0
Masato Max Yamamoto <masato.yamamoto10@gmail.com>
2018-09-01 23:10:57 -0500

    test

--- GetEDINET.py
+++ GetEDINET.py
@@ -1,3 +1,10 @@
 from edinet_xbrl.edinet_xbrl_downloader import EdinetXbrlDownloader
 
+# Download SC's XBRL
+# init downloader
+xbrl_downloader = EdinetXbrlDownloader()
 
+## set a ticker you want to download xbrl file
+ticker = "8053"
+target_dir = "."
+xbrl_downloader.download_by_ticker(ticker, target_dir)
