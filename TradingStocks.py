ab4a634602fae7712ccea9b95e9ca6ef8b08ec15
Masato Max Yamamoto <masato.yamamoto10@gmail.com>
2018-09-24 01:42:14 -0500

    a little amendment

--- TradingStocks.py
+++ TradingStocks.py
@@ -15,14 +15,19 @@ rcParams['figure.figsize'] = 15, 6
 
 start = datetime.date(2010, 5, 16)
 end = datetime.date(2018, 7, 18)
-df = web.DataReader(["NIKKEI225","WILLREITIND"], "fred", start, end)
+df = web.DataReader(["NIKKEI225", "WILLREITIND"], "fred", start, end)
 
 df_diff = df.pct_change()
+
+print(df_diff)
+df_diff.plot()
 
 # US REITの分析
 # https://www.mazarimono.net/entry/2018/07/20/pandas_datareader
 
 df_diff_2 = df_diff.rolling(20).std() * np.sqrt(365)
 df_diff_2 = df_diff_2.dropna()
-df_diff_2.plot()
+df_diff_1 = df_diff["WILLREITIND"]
+
+df_diff_1_acf = sm.tsa.stattools.acf(df_diff_1, nlags=40)
 plt.show()
